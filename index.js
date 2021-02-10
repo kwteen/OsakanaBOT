aeb02da24b254fb291b02dcf14fa0ddb218ceeae
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-02-10 21:59:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ const fs = require('fs')
 const figlet = require('figlet')
 const lolcatjs = require('lolcatjs')
 const color = require('./lib/color')
+
+const setting = JSON.parse(fs.readFileSync('./settings/setting.json'))
+//THANKS TO TOBZ
+lolcatjs.options.seed = Math.round(Math.random() * 1000);
+lolcatjs.options.colors = true;
+
+const start = async (nzwa = new Client()) => {
+        console.log('------------------------------------------------')
+        lolcatjs.fromString(color(figlet.textSync('OSAKANA BOT', { horizontalLayout: 'full' })))
+        console.log('------------------------------------------------')
+        lolcatjs.fromString('[DEV] NAZWAS')
+        lolcatjs.fromString('[SYSTEM] BOT Started!')
+        tobz.onAnyMessage((fn) => messageLog(fn.fromMe, fn.type))
+        // Force it to keep the current session
+        nzwa.onStateChanged((state) => {
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') tobz.forceRefocus()
+        })
+
+}

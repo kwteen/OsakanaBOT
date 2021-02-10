bc03f90495903f79e3cfbf88e46a88f573334194
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-02-10 21:09:41 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const {
+    create,
+    Client
+} = require('@open-wa/wa-automate')
+const cron = require('node-cron')
+const fs = require('fs')
+const figlet = require('figlet')
+const lolcatjs = require('lolcatjs')

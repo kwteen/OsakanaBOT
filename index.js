6212e6841e445ffd04602efceadb0e7ffb6bf2b1
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-02-10 22:16:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ const lolcatjs = require('lolcatjs')
 const color = require('./lib/color')
 
 const setting = JSON.parse(fs.readFileSync('./settings/setting.json'))
-//THANKS TO TOBZ
+
+/*
+//THANKS TO TOBZ & NURUTOMO
+require('./nzwa.js')
+nocache('./nzwa.js', module => console.log(`'${module}' Updated!`))
+require('./src/menu/help.js')
+nocache('./src/menu/help.js', module => console.log(`'${module}' Updated!`))
+require('./settings/setting.json')
+nocache('./settings/setting.json', module => console.log(`'${module}' Updated!`))
+*/
+
 lolcatjs.options.seed = Math.round(Math.random() * 1000);
 lolcatjs.options.colors = true;
 
@@ -19,11 +29,30 @@ const start = async (nzwa = new Client()) => {
         console.log('------------------------------------------------')
         lolcatjs.fromString('[DEV] NAZWAS')
         lolcatjs.fromString('[SYSTEM] BOT Started!')
-        tobz.onAnyMessage((fn) => messageLog(fn.fromMe, fn.type))
+        nzwa.onAnyMessage((fn) => messageLog(fn.fromMe, fn.type))
         // Force it to keep the current session
         nzwa.onStateChanged((state) => {
             console.log('[Client State]', state)
             if (state === 'CONFLICT' || state === 'UNLAUNCHED') tobz.forceRefocus()
         })
+        // listening on message
+        nzwa.onMessage((async (message) => {
 
+        nzwa.getAmountOfLoadedMessages() // Cut message Cache if cache more than 3K
+            .then((msg) => {
+                if (msg >= 1000) {
+                    console.log('[CLIENT]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    nzwa.cutMsgCache()
+                }
+            })
+        // msgHndlr(nzwa, message)
+        // Message Handler (Loaded from recent cache)
+        require('./nzwa.js')(nzwa, message)
+    }))
+           
+
+        nzwa.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(nzwa, heuh) 
+            left(nzwa, heuh)
+            }))
 }
